51d4a4a115c1d39290c8ade39ccae27f9957c3cb
iKulakov5 <codykulakov@gmail.com>
2019-11-15 23:41:29 +0300

    hw done

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -11,8 +13,33 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    if(typeof percent !== 'number' && typeof percent !== 'string') {
+        console.log(`Параметр процентная ставка содержит неправильное значение ${percent}`)
+    }
+    if(typeof contribution !== 'number' && typeof contribution !== 'string') {
+        console.log(`Параметр первоначальный взнос содержит неправильное значение ${contribution}`)
+    }
+    if(typeof amount !== 'number' && typeof amount !== 'string') {
+        console.log(`Параметр общая стоимость содержит неправильное значение ${amount}`)
+    }
+    if(typeof date !== 'number' && typeof date !== 'string') {
+        console.log(`Параметр срок ипотеки содержит неправильное значение ${date}`)
+    }
+
+    let S = amount - contribution;
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let currentMonth = currentDate.getMonth() + 1;
+    let endYear = parseInt(date.slice(0, 4));
+    let endMonth = parseInt(date.slice(5, 7));
+    let totalMonth = ((endYear - currentYear) * 12) + (endMonth - currentMonth);
+    let P = percent / 100 / 12;
+    let monthPayment = S * (P + P / (((1 + P) ** totalMonth) - 1))
+    let totalAmount = monthPayment * totalMonth;
+
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +50,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+    if(name === "" || name == null || name == undefined) {
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    }
+    console.log(greeting);
+
+    return greeting;
 }
